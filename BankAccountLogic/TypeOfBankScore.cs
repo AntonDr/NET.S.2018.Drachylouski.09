37a360ff3f9cd001a8f201c5775682d18c6d2160
AntonDr <31773079+AntonDr@users.noreply.github.com>
2018-07-15 18:22:41 +0300

    done

---
+++ BankAccountLogic/TypeOfBankScore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancAccountLogic
+{
+    public enum TypeOfBankScore
+    {
+        Base = 1,
+        Silver,
+        Gold,
+        Platinum
+    }
+}
